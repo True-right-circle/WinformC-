06c7c878fa3a0ff782e954569d2f3018299f6980
woowon123 <wlsdndnjs12@naver.com>
2017-05-30 17:54:10 +0900

    Faker Game early stage

---
+++ Winform_Faker_Game/Winform_Faker_Game/DubbleBufferedPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winform_Faker_Game
+{
+    class DubbleBufferedPanel:System.Windows.Forms.Panel
+    {
+        public DubbleBufferedPanel()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
---
+++ Winform_Faker_Game/Winform_Faker_Game/Form.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_Faker_Game
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            bg = Properties.Resources.bg1;
+        }
+        Image bg;
+
+        private void dubbleBufferedPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bg, ClientRectangle);
+        }
+    }
+}
